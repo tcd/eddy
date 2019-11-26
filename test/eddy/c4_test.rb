637ede96da760de421f11501d2a374960fa4c143
Clay Dunston <claydunston@gmail.com>
2019-11-26 16:21:04 -0600

    Updates for 0.1.0

--- test/eddy/c4_test.rb
+++
@@ -1,9 +0,0 @@
-require "test_helper"
-
-class EddyTest < Minitest::Test
-
-  def test_that_it_has_a_version_number
-    refute_nil(Eddy::VERSION)
-  end
-
-end
---
+++ test/eddy/eddy_test.rb
@@ -0,0 +1,22 @@
+require "test_helper"
+require "rubygems"
+
+class EddyTest < Minitest::Test
+
+  def test_that_it_has_a_version_number
+    refute_nil(Eddy::VERSION)
+  end
+
+  def test_that_versions_match
+    spec_path = File.join(Eddy.root_dir, "eddy.gemspec")
+    spec = Gem::Specification.load(spec_path)
+    assert_equal(Eddy::VERSION, spec.version.to_s)
+  end
+
+  def test_readme_links_to_correct_version
+    search_string = "[docs]: https://www.rubydoc.info/gems/eddy/#{Eddy::VERSION}"
+    readme = File.read(File.join(Eddy.root_dir, "readme.md"))
+    assert(readme.include?(search_string))
+  end
+
+end
