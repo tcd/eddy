ecf9b8c6cff629e432d583e1d7ee98b7b94b4498
Clay Dunston <claydunston@gmail.com>
2019-12-02 18:36:51 -0600

    Add `SegmentNote` class

---
+++ lib/eddy/segment/segment_note.rb
@@ -0,0 +1,8 @@
+module Eddy
+  module Segment
+    # Three types of segment level notes:
+    # - Syntax:  Define dependencies based on the presence or absence of other data elements in the segment
+    # - Semantic:  Provide additional information about the data element including any dependence based on the data value in another data element in the segment
+    # - Comments:  Clarify the intended use of the segment - comments are not part of the standard
+  end
+end
