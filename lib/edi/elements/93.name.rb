c23a9f31d320d0e681e578984dfdcbc86adb9d8d
Clay Dunston <claydunston@gmail.com>
2019-12-05 09:51:23 -0600

    Add new element

---
+++ lib/edi/elements/93.name.rb
@@ -0,0 +1,19 @@
+module Eddy
+  module Elements
+    # - Id: 93
+    # - Name: Name
+    # - Type: AN
+    # - Min/Max: 1/60
+    # - Description: Free-form name
+    class Name < Eddy::Element::AN
+      # @return [void]
+      def initialize()
+        @id = "93"
+        @name = "Name"
+        @type = "AN"
+        self.min = 1
+        self.max = 60
+      end
+    end
+  end
+end
