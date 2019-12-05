0a29f7f3c1a1e9e21c81a1908f456c19841a60c0
Clay Dunston <claydunston@gmail.com>
2019-12-04 21:45:35 -0600

    Add `Def`

---
+++ lib/eddy/def.rb
@@ -0,0 +1,8 @@
+module Eddy
+  # Classes in `Def` provide descriptions of EDI components.
+  module Def; end
+end
+
+require "eddy/def/element_summary"
+require "eddy/def/loop_summary"
+require "eddy/def/segment_summary"
