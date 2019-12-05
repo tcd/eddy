c7de4fc16b2f938e853d098ae50667e46184339c
Clay Dunston <claydunston@gmail.com>
2019-12-05 11:30:03 -0600

    Work

--- lib/eddy.rb
+++ lib/eddy.rb
@@ -9,4 +9,9 @@ require "eddy/segment"
 require "eddy/transaction_set"
 require "eddy/build/build_elements"
 
-Dir.glob(File.join(__dir__, "edi", "**/*.rb")).each { |file| require file }
+Dir.glob(File.join(__dir__, "edi", "elements", "*.rb")).each { |file| require file }
+Dir.glob(File.join(__dir__, "edi", "elements", "test", "*.rb")).each { |file| require file }
+Dir.glob(File.join(__dir__, "edi", "segments", "*.rb")).each { |file| require file }
+Dir.glob(File.join(__dir__, "edi", "transaction_sets", "*.rb")).each { |file| require file }
+
+# Dir.glob(File.join(__dir__, "edi", "**/*.rb")).each { |file| require file }
