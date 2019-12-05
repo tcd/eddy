4399dfdd0cb482657cd7199a9849c29cecc629c7
Clay Dunston <claydunston@gmail.com>
2019-12-05 11:30:21 -0600

    Add segment

---
+++ lib/edi/segments/n2.rb
@@ -0,0 +1,31 @@
+module Eddy
+  module Segments
+    # Id: N2
+    # Description: Additional Name Information
+    class N2 < Eddy::Segment::Base
+
+      attr_reader :name_1
+      attr_reader :name_2
+
+      # @return [void]
+      def initialize()
+        self.id = "N2"
+        self.description = "Additional Name Information"
+        self.elements = ["93", "93"]
+        @name_1 = Eddy::Elements::Name.new()
+        @name_2 = Eddy::Elements::Name.new()
+      end
+
+      # (see Eddy::Elements::Name)
+      def name_1=(val)
+        @name_1.value = val
+      end
+
+      # {include:Eddy::Elements::Name}
+      def name_2=(val)
+        @name_1.value = val
+      end
+
+    end
+  end
+end
