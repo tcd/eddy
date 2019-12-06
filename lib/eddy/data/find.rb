c50c951cff83de48ec8d653908c19de17e7b179b
Clay Dunston <claydunston@gmail.com>
2019-12-05 23:27:30 -0600

    Implement find_element_by_id

--- lib/eddy/data/find.rb
+++ lib/eddy/data/find.rb
@@ -1,9 +1,13 @@
 module Eddy
   module Data
+    # Return the full name of an element with the given id.
+    #
     # @param id [String] ID of the Element to look up.
     # @return [Hash]
     def self.find_element_by_id(id)
-      data = Eddy::Build.generate_element_data()
+      data = Eddy::Data.element_ids()
+      return data[id] if data.key?(id)
+      raise Eddy::Errors::Error, "No element found with id #{id}"
     end
   end
 end
