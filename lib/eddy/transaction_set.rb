3be4053db533ae20f952987e0637988d2ef6d83b
Clay Dunston <claydunston@gmail.com>
2019-12-05 20:19:50 -0600

    Move `TransactionSet` to a single file

--- lib/eddy/transaction_set.rb
+++ lib/eddy/transaction_set.rb
@@ -1,6 +1,34 @@
 module Eddy
+  # Base class for EDI Transaction Sets.
   # Top level EDI document.
-  module TransactionSet; end
-end
+  # Interchange / Document / Envelope / Group / File
+  class TransactionSet
+    # @return [Integer]
+    attr_accessor :code
+    # @return [String]
+    attr_accessor :functional_group
+    # @return [String]
+    attr_accessor :description
+    # @return [Array<Segment>]
+    attr_accessor :segments
+    # Time value for use by DT/TM Elements.
+    # @return [Time]
+    attr_accessor :time
+    # Component Element Separator
+    # @return [String] (">")
+    attr_accessor :component_separator
+    # @return [String] ("~")
+    attr_accessor :segment_separator
+    # Data Element Separator
+    # @return [String] ("*")
+    attr_accessor :element_separator
 
-require "eddy/transaction_set/base"
+    # @return [void]
+    def initialize()
+      self.time = Time.now().utc()
+      self.component_separator = ">"
+      self.segment_separator = "~"
+      self.element_separator = "*"
+    end
+  end
+end
