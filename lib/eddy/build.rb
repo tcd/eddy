1d7ab793689f9b71a0413f64cb6807640c3d917a
Clay Dunston <claydunston@gmail.com>
2019-12-05 23:27:05 -0600

    Remove unused code

--- lib/eddy/build.rb
+++ lib/eddy/build.rb
@@ -7,6 +7,7 @@ require "eddy/build/tm"
 require "eddy/build/segments"
 
 module Eddy
+  # Generate Ruby classes for modeling Data Element logic, notes, and validation.
   module Build
 
     # Create folders in `build/elements`.
@@ -14,8 +15,7 @@ module Eddy
     # @return [void]
     def self.make_element_folders()
       build_dir = File.join(Eddy.root_dir, "build")
-      dirs = ["an", "dt", "id", "n", "r", "tm"]
-      dirs.each do |dir|
+      ["an", "dt", "id", "n", "r", "tm"].each do |dir|
         FileUtils.mkdir_p(File.join(build_dir, "elements", dir))
       end
       return nil
@@ -25,8 +25,7 @@ module Eddy
     #
     # @return [void]
     def self.clean_element_folders()
-      dirs = ["an", "dt", "id", "n", "r", "tm"]
-      dirs.each do |dir|
+      ["an", "dt", "id", "n", "r", "tm"].each do |dir|
         files = Dir.glob(File.join(Eddy.root_dir, "build", "elements", dir, "*.rb"))
         File.delete(*files)
       end
@@ -38,56 +37,19 @@ module Eddy
     # @return [Array<Hash>]
     def self.generate_elements()
       elements = self.generate_element_data()
-      elements.each do |e|
-        case e[:type]
-        when "AN" then self.build_an(e)
-        when "B" then self.build_b(e)
-        when "DT" then self.build_dt(e)
-        when "ID" then self.build_id(e)
-        when /N\d*/ then self.build_n(e)
-        # when /R\d*/
-        when "R" then self.build_r(e)
-        when "TM" then self.build_tm(e)
+      elements.each do |el|
+        case el[:type]
+        when "AN"   then self.build_an(el)
+        when "B"    then self.build_b(el)
+        when "DT"   then self.build_dt(el)
+        when "ID"   then self.build_id(el)
+        when /N\d*/ then self.build_n(el)
+        when "R"    then self.build_r(el)
+        when "TM"   then self.build_tm(el)
         else
           raise Eddy::Error "unable to determine element type"
         end
       end
-      return nil
-    end
-
-    # @deprecated Use specific build functions instead.
-    #
-    # @param e [Hash]
-    # @return [void]
-    def self.build_element(e)
-      constructor = Ginny::Func.create({
-        name: "initialize",
-        body: <<~FUNC_BODY,
-          @id = "#{e[:id]}"
-          @name = "#{e[:name]}"
-          @type = "#{e[:type]}"
-          self.min = #{e[:min]}
-          self.max = #{e[:max]}
-        FUNC_BODY
-      }).render()
-      data = {
-        name: e[:name],
-        description: e[:description],
-        parent: "Eddy::Element::#{e[:type]}",
-        modules: ["Eddy", "Elements"],
-        body: constructor,
-        file_prefix: "#{e[:id]}.",
-      }
-      c = Ginny::Class.create(data)
-      c.generate(File.join(Eddy.root_dir, "build", "elements"))
-      return nil
-    end
-
-    # Not sure how to handle this bad boy yet.
-    #
-    # @param e [Hash]
-    # @return [void]
-    def self.build_b(e)
       return nil
     end
 
