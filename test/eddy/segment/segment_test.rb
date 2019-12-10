7268ae72c5cc2ef2aba6a3dacd2d16587b31daae
Clay Dunston <claydunston@gmail.com>
2019-12-09 19:44:24 -0600

    Update segment test

--- test/eddy/segment/segment_test.rb
+++ test/eddy/segment/segment_test.rb
@@ -2,9 +2,17 @@ require "test_helper"
 
 class SegmentTest < Minitest::Test
 
-  def test_base
+  def test_n2
     x = Eddy::Segments::N2.new()
     x.name_1 = "x"
     assert_equal("N2*x", x.render("*"))
+  end
+
+  def test_isa
+    skip()
+    isa = Eddy::Segments::ISA.new()
+    isa.AuthorizationInformationQualifier = ""
+    isa.InterchangeDate = "2"
+    assert_equal("", isa.render("*"))
   end
 end
