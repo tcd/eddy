6965fb4fae3afd335c68b18afc2ca5952bc34264
Clay Dunston <claydunston@gmail.com>
2019-12-10 11:53:43 -0600

    Add new element

---
+++ lib/definitions/elements/manual/unique_value/350.an.assigned_identification.rb
@@ -0,0 +1,19 @@
+module Eddy
+  module Elements
+    # ### Element Summary:
+    #
+    # - Id: 350
+    # - Name: Assigned Identification
+    # - Type: AN
+    # - Min/Max: 1/20
+    # - Description: Alphanumeric characters assigned for differentiation within a transaction set
+    class AssignedIdentification < Eddy::Element::AN
+      # @return [void]
+      def initialize()
+        @id = "350"
+        @name = "Assigned Identification"
+        super(min: 1, max: 20)
+      end
+    end
+  end
+end
