0c30db561ab4189c8bea790806b2930194ae2354
Clay Dunston <claydunston@gmail.com>
2019-12-10 23:10:56 -0600

    Update some DT/TM elements

--- lib/definitions/elements/generated/337.tm.time.rb
+++ lib/definitions/elements/generated/337.tm.time.rb
@@ -8,11 +8,17 @@ module Eddy
     # - Min/Max: 4/8
     # - Description: Time expressed in 24-hour clock time as follows: HHMM, or HHMMSS, or HHMMSSD, or HHMMSSDD, where H = hours (00-23), M = minutes (00-59), S = integer seconds (00-59) and DD = decimal seconds; decimal seconds are expressed as follows: D = tenths (0-9) and DD = hundredths (00-99)
     class E337 < Eddy::Element::TM
+      # @param time [Time]
       # @return [void]
-      def initialize()
+      def initialize(time)
         @id = "337"
         @name = "Time"
-        super(min: 4, max: 8, fmt: :hhmmssdd)
+        super(
+          min: 4,
+          max: 8,
+          fmt: :hhmmssdd,
+          val: time,
+        )
       end
     end
   end
--- lib/definitions/elements/generated/373.dt.date.rb
+++ lib/definitions/elements/generated/373.dt.date.rb
@@ -8,11 +8,17 @@ module Eddy
     # - Min/Max: 8/8
     # - Description: Date expressed as CCYYMMDD
     class E373 < Eddy::Element::DT
+      # @param time [Time]
       # @return [void]
-      def initialize()
+      def initialize(time)
         @id = "373"
         @name = "Date"
-        super(min: 8, max: 8, fmt: :ccyymmdd)
+        super(
+          min: 8,
+          max: 8,
+          fmt: :ccyymmdd,
+          val: time,
+        )
       end
     end
   end
