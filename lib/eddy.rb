f59d322ccadc5d3c9e88106bfd96e4b2acb5bdee
Clay Dunston <claydunston@gmail.com>
2019-12-14 21:00:27 -0600

    Remove commented code

--- lib/eddy.rb
+++ lib/eddy.rb
@@ -16,8 +16,6 @@ Dir.glob(File.join(__dir__, "eddy", "build", "elements", "*.rb")).each { |file| 
 require "eddy/build/elements"
 require "eddy/build/segments"
 
-# Dir.glob(File.join(__dir__, "definitions", "elements", "*.rb")).each { |file| require file }
 Dir.glob(File.join(__dir__, "definitions", "elements", "manual", "**", "*.rb")).each { |file| require file }
-# Dir.glob(File.join(__dir__, "definitions", "segments", "*.rb")).each { |file| require file }
 Dir.glob(File.join(__dir__, "definitions", "segments", "manual", "**", "*.rb")).each { |file| require file }
 Dir.glob(File.join(__dir__, "definitions", "transaction_sets", "*.rb")).each { |file| require file }
