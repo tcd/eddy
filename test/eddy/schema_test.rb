9b1c86b870404002a91cf82b495cb490b0b6919c
Clay Dunston <claydunston@gmail.com>
2019-12-16 15:40:18 -0600

    Remove mystery characters, save them in a test.

--- test/eddy/schema_test.rb
+++ test/eddy/schema_test.rb
@@ -10,4 +10,24 @@ class SchemaTest < Minitest::Test
     skip()
   end
 
+  def test_doc_comment
+  skip("There be mystery characters in these strings...")
+    yml = <<~YAML
+      ---
+      id: IEA
+      name: Interchange Control Trailer
+      purpose: ​To define the end of an interchange of zero or more functional groups and interchange-related control segments.
+      elements:
+        - { ref: IEA01, id: 'I16' }
+        - { ref: IEA02, id: 'I12' }
+    YAML
+    want = <<~STR
+      # ### Segment Summary:
+      #
+      # - Id: IEA
+      # - Name: Interchange Control Trailer
+      # - Purpose: ​To define the end of an interchange of zero or more functional groups and interchange-related control segments.
+    STR
+  end
+
 end
