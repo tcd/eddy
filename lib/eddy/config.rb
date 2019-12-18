65e1053dc649e30470670f775716f0176945ffc2
Clay Dunston <claydunston@gmail.com>
2019-12-18 15:56:42 -0600

    Add config class

---
+++ lib/eddy/config.rb
@@ -0,0 +1,19 @@
+module Eddy
+  # Configuration for Eddy
+  class Config
+    # test, development, or production
+    # @return [String]
+    attr_accessor :env
+
+    # :memory, :file, :db?
+    # @return [Symbol]
+    attr_accessor :persistence_method
+
+    # @return [void]
+    def initialize()
+      self.env = "test"
+      self.persistence_method = :memory
+    end
+
+  end
+end
