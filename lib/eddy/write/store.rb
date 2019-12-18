224faef1d26e4e3fed7396201389adfccf19044e
Clay Dunston <claydunston@gmail.com>
2019-12-17 18:32:33 -0600

    Add attributes to `Store`

--- lib/eddy/write/store.rb
+++ lib/eddy/write/store.rb
@@ -4,10 +4,16 @@ module Eddy
   # Container used to distribute state throughout an Interchange.
   class Store
 
-    # @return [String] ("")
-    attr_accessor :edi_version
+    # Time value for use by DT/TM Elements.
+    # @return [Time]
+    attr_accessor :time
 
     # Component Element Separator
+    #
+    # Used by:
+    #
+    # - Eddy::Elements::I15
+    #
     # @return [String] (">")
     attr_accessor :component_separator
     # @return [String] ("~")
@@ -16,9 +22,21 @@ module Eddy
     # @return [String] ("*")
     attr_accessor :element_separator
 
-    # Time value for use by DT/TM Elements.
-    # @return [Time]
-    attr_accessor :time
+    # Used by:
+    #
+    # - Eddy::Elements::I11
+    #
+    # @return [String] ("00401")
+    attr_accessor :edi_version
+
+    # Version / Release / Industry Identifier Code
+    #
+    # Used by:
+    #
+    # - Eddy::Elements::E480
+    #
+    # @return [String]
+    attr_accessor :version_release_industry_id_code
 
     # Total number of transaction sets included in the functional group or interchange (transmission) group.
     # @return [Integer]
@@ -27,6 +45,15 @@ module Eddy
     # @return [Integer]
     attr_accessor :number_of_included_functional_groups
 
+    # Application Sender's Code
+    # Code identifying party sending transmission; codes agreed to by trading partners
+    # @return [String]
+    attr_accessor :application_senders_code
+    # Application Receiver's Code
+    # Code identifying party receiving transmission. Codes agreed to by trading partners
+    # @return [String]
+    attr_accessor :application_receivers_code
+
     # @param time [Time]
     # @return [void]
     def initialize(time: Time.now().utc())
@@ -34,8 +61,12 @@ module Eddy
       self.component_separator = ">"
       self.segment_separator   = "~"
       self.element_separator   = "*"
+      self.edi_version = "00401"
       self.number_of_included_functional_groups = 0
-      self.number_of_transaction_sets_included = 0
+      self.number_of_transaction_sets_included  = 0
+      self.application_senders_code = "sender_id"
+      self.application_receivers_code = "receiver_id"
+      self.version_release_industry_id_code = "004010"
       @group_control_number = 1
     end
 
