55d54fd47a3956f3300f5c518641f5e8379104e5
Clay Dunston <claydunston@gmail.com>
2019-12-18 20:47:11 -0600

    Add Composite placeholder

---
+++ lib/eddy/models/element/composite.rb
@@ -0,0 +1,6 @@
+module Eddy
+  module Element
+    # - [References to Component Data Elements - X12 RFI](http://www.x12.org/rfis/References%20to%20Component%20Data%20Elements.pdf)
+    class Composite; end
+  end
+end
