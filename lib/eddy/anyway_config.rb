3cd86cae749a5f9cec2f2056672258b383be93d8
Clay Dunston <claydunston@gmail.com>
2019-12-19 15:05:53 -0600

    Add docs for `Config`

--- lib/eddy/anyway_config.rb
+++ lib/eddy/anyway_config.rb
@@ -4,8 +4,16 @@ module Eddy
   # Configuration for Eddy
   class Config < Anyway::Config
     config_name :eddy
+
+    # @!attribute [rw] env
+    #   @return [String<"test", "development", "production">] ("development")
     attr_config env: "development"
+
+    # @!attribute [rw] persistence_method
+    #   Method used by Eddy to persist data about EDI control numbers.
+    #   @return [Symbol<:memory, :file>] (:memory)
     attr_config persistence_method: :memory
+
     attr_config tmp_dir: File.join(Eddy::Util.root_dir, "tmp")
     attr_config build_dir: File.join(Eddy::Util.root_dir, "build")
   end
