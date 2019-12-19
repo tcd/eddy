1d0088a87d124a63fd982a25031a734458503411
Clay Dunston <claydunston@gmail.com>
2019-12-19 16:58:37 -0600

    Add back methods for making/cleaning build folders

--- lib/eddy/build/build.rb
+++ lib/eddy/build/build.rb
@@ -1,6 +1,28 @@
+require "fileutils"
+
 module Eddy
   # Generate Ruby classes for modeling Semgents & Data Element logic, notes, and validation.
-  module Build; end
+  module Build
+
+    # @return [void]
+    def self.make_folders()
+      folders = [
+        File.join(Eddy.config.build_dir, "segments"),
+        File.join(Eddy.config.build_dir, "elements"),
+      ]
+      folders.each { |f| FileUtils.mkdir_p(f) }
+    end
+
+    # @return [void]
+    def self.clean_folders()
+      folders = [
+        File.join(Eddy.config.build_dir, "segments"),
+        File.join(Eddy.config.build_dir, "elements"),
+      ]
+      folders.each { |f| Eddy::Util.clean_folder(f) }
+    end
+
+  end
 end
 
 require "eddy/build/elements/an"
