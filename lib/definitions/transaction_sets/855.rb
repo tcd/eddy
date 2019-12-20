f5802da569bda5aea723344ac4070dc7636067c9
Clay Dunston <claydunston@gmail.com>
2019-12-20 11:19:40 -0600

    [WIP] Loop implementation

--- lib/definitions/transaction_sets/855.rb
+++ lib/definitions/transaction_sets/855.rb
@@ -1,5 +1,55 @@
 module Eddy
   module TransactionSets
+    class L_N1 < Eddy::Loop::Base
+      # @param store [Eddy::Data::Store]
+      # @return [void]
+      def initialize(store)
+        self.store = store
+        @loop_id = "L_N1"
+        @repeat = 200
+        self.components = [
+          Eddy::Segments::N1,
+          Eddy::Segments::N3,
+          Eddy::Segments::N4,
+        ]
+      end
+    end
+    class L_PO1 < Eddy::Loop::Base
+      # @param store [Eddy::Data::Store]
+      # @return [void]
+      def initialize(store)
+        self.store = store
+        @loop_id = "L_PO1"
+        @repeat = 100_000
+        self.components = [
+          Eddy::Segments::PO1,
+          Eddy::TransactionSets::L_PID,
+          Eddy::TransactionSets::L_ACK,
+        ]
+      end
+    end
+    class L_PID < Eddy::Loop::Base
+      # @param store [Eddy::Data::Store]
+      # @return [void]
+      def initialize(store)
+        self.store = store
+        @loop_id = "L_PID"
+        @repeat = 1000
+        self.components = []
+      end
+    end
+    class L_ACK < Eddy::Loop::Base
+      # @param store [Eddy::Data::Store]
+      # @return [void]
+      def initialize(store)
+        self.store = store
+        @loop_id = "L_ACK"
+        @repeat = 104
+        self.components = [
+          Eddy::Segments::ACK,
+        ]
+      end
+    end
     # ### Transaction Set Summary:
     #
     # - Id: 855
@@ -11,9 +61,14 @@ module Eddy
       NAME = "Purchase Order Acknowledgemen".freeze
       FUNCTIONAL_GROUP = "PR".freeze
 
-      # @param store [Eddy::Store]
+      # @param store [Eddy::Data::Store]
       # @return [void]
       def initialize(store)
+        self.store = store
+        @bak = nil
+        @l_n1 = nil
+        @l_po1 = nil
+        @ctt = nil
         super(
           store,
         )
