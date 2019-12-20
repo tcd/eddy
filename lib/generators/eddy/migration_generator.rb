3cbc17239ad35072838bef1040b9a852070d21bf
Clay Dunston <claydunston@gmail.com>
2019-12-20 11:30:26 -0600

    Update migration generator

--- lib/generators/eddy/migration_generator.rb
+++ lib/generators/eddy/migration_generator.rb
@@ -2,7 +2,14 @@ require "rails/generators"
 
 module Eddy
   module Generators
+    # Generate migrations to set up database tables for use by Eddy.
+    #
+    # See:
+    #
+    # - https://api.rubyonrails.org/classes/Rails/Generators/Migration.html
     class MigrationGenerator < Rails::Generators::Base
+      include Rails::Generators::Migration
+      source_root File.expand_path("templates", __dir__)
 
     end
   end
