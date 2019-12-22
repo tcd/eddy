69773329f5b1008041f2a9885b7aa38d3354b2b5
Clay Dunston <claydunston@gmail.com>
2019-12-21 18:26:59 -0600

    Update util/normalize.rb

--- lib/eddy/util/normalize.rb
+++ lib/eddy/util/normalize.rb
@@ -1,6 +1,8 @@
 module Eddy
   module Util
 
+    # Given an Element Id (positive number under 1688, or I01-I64), returns a string sutable for use as a Ruby class name.
+    #
     # @param id [String]
     # @return [String]
     def self.normalize_id(id)
@@ -18,7 +20,8 @@ module Eddy
       end
     end
 
-    # Remove dashes, underscores, spaces, slashes, periods, and parens from a string then titleize it.
+    # Convert a string to [*PascalCase*](http://wiki.c2.com/?PascalCase), or *UpperCamelCase*
+    # Remove dashes, slashes, underscores, spaces, periods, and parens from a string then titleize it.
     #
     # @param name [String]
     # @return [String]
@@ -28,6 +31,18 @@ module Eddy
                  .split(" ")
                  .map(&:capitalize)
                  .join("")
+    end
+
+    # Convert a string to [*snake_case*](https://en.wikipedia.org/wiki/Snake_case)
+    #
+    # @param name [String]
+    # @return [String]
+    def self.snake_case(name)
+      return name.gsub(/\s*\(.*\)|['.]/, "")
+                 .gsub(%r{[,_\-/]}, " ")
+                 .split(" ")
+                 .map(&:downcase)
+                 .join("_")
     end
 
   end
--- test/eddy/util/normalize_test.rb
+++ test/eddy/util/normalize_test.rb
@@ -9,6 +9,38 @@ module UtilTest
       assert_equal("I420", Eddy::Util.normalize_id("I420"))
     end
 
+    def test_snake_case
+      cases = {
+        "Number of Accepted Transaction Sets" => "number_of_accepted_transaction_sets",
+        "Free Form Message" => "free_form_message",
+        "Free-Form Message" => "free_form_message",
+        "Pick-up Date" => "pick_up_date",
+        "Harbor Maintenance Fee (HMF) Exemption Code" => "harbor_maintenance_fee_exemption_code",
+        "Product/Service Substitution Code" => "product_service_substitution_code",
+        "Bill of Lading/Waybill Number" => "bill_of_lading_waybill_number",
+        "Pre-Cooled (Rule 710) Code" => "pre_cooled_code",
+        "Surface/Layer/Position Code" => "surface_layer_position_code",
+        "U.P.C./EAN Consumer Package Code" => "upc_ean_consumer_package_code",
+        "D-U-N-S Number" => "d_u_n_s_number", # TODO: Don't like this result.
+        "Service, Promotion, Allowance, or Charge Code" => "service_promotion_allowance_or_charge_code",
+        "Shipper's Export Declaration Requirements" => "shippers_export_declaration_requirements",
+        "Resource Code (or Identifier)" => "resource_code",
+        "Interchange ID Qualifier" => "interchange_id_qualifier",
+        "Length of Binary Data" => "length_of_binary_data",
+      }
+      cases.each do |key, val|
+        want = val
+        input = key
+        have = Eddy::Util.snake_case(input)
+        message = <<~END
+          input:  #{input}
+          wanted: #{want}
+          have:   #{have}
+        END
+        assert_equal(want, have, message)
+      end
+    end
+
     def test_normalize_name
       cases = {
         "Number of Accepted Transaction Sets" => "NumberOfAcceptedTransactionSets",
@@ -20,7 +52,6 @@ module UtilTest
         "Bill of Lading/Waybill Number" => "BillOfLadingWaybillNumber",
         "Pre-Cooled (Rule 710) Code" => "PreCooledCode",
         "Surface/Layer/Position Code" => "SurfaceLayerPositionCode",
-        # "U.P.C./EAN Consumer Package Code" => "UPCEANConsumerPackageCode",
         "U.P.C./EAN Consumer Package Code" => "UPCEanConsumerPackageCode",
         "D-U-N-S Number" => "DUNSNumber",
         "Service, Promotion, Allowance, or Charge Code" => "ServicePromotionAllowanceOrChargeCode",
