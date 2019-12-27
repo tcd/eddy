7da37bbc18dae983ed41069cba8339429c0bc040
Clay Dunston <claydunston@gmail.com>
2019-12-27 10:47:17 -0600

    Add `E1301`

---
+++ lib/definitions/elements/generated/1301.agency_service_promotion_allowance_or_charge_code.rb
@@ -0,0 +1,22 @@
+module Eddy
+  module Elements
+    # ### Element Summary:
+    #
+    # - Id: 1301
+    # - Name: Agency Service, Promotion, Allowance, or Charge Code
+    # - Type: AN
+    # - Min/Max: 1/10
+    # - Description: Agency maintained code identifying the service, promotion, allowance, or charge
+    class E1301 < Eddy::Element::AN
+      # @param val [String]
+      # @param req [String]
+      # @return [void]
+      def initialize(val: nil, req: nil)
+        @id = "1301"
+        @name = "Agency Service, Promotion, Allowance, or Charge Code"
+        @description = "Agency maintained code identifying the service, promotion, allowance, or charge"
+        super(min: 1, max: 10, req: req, val: val)
+      end
+    end
+  end
+end
