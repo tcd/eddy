744de0d750a54389bd4e34a54dfbc33976257d09
Clay Dunston <claydunston@gmail.com>
2019-12-28 12:15:12 -0600

    Refactor some model tests

--- test/eddy/models/element/an_test.rb
+++ test/eddy/models/element/an_test.rb
@@ -3,38 +3,43 @@ require "test_helper"
 module ModelsTest
 
   # Example class for testing inheritence of `Eddy::Element::AN`.
-  class ExampleAnElement < Eddy::Element::AN
+  class ExampleAN < Eddy::Element::AN
     def initialize(val: nil, req: nil)
-      @id = @name = @description = "Example"
-      super(min: 1, max: 10, req: req, val: val)
+      @id = @name = @description = @ref = "Example"
+      super(
+        min: 1,
+        max: 10,
+        req: req,
+        val: val,
+      )
     end
   end
 
   class AnTest < Minitest::Test
 
     def test_setter
-      an = ExampleAnElement.new()
+      an = ExampleAN.new()
       an.value = "Clay"
       assert_equal("Clay", an.value)
     end
 
     def test_value_setter_validates_length
-      an = ExampleAnElement.new()
+      an = ExampleAN.new()
       assert_raises(Eddy::Errors::ElementValidationError) { an.value = "Clay123456789101010" }
     end
 
     def test_value_getter_when_mandatory
-      an = ExampleAnElement.new(val: "Clay", req: "M")
+      an = ExampleAN.new(val: "Clay", req: "M")
       assert_equal("Clay", an.value)
     end
 
     def test_value_getter_when_mandatory_and_nil
-      an = ExampleAnElement.new(req: "M")
+      an = ExampleAN.new(req: "M")
       assert_raises(Eddy::Errors::ElementNilValueError) { an.value() }
     end
 
     def test_value_getter_when_optional_and_nil
-      an = ExampleAnElement.new()
+      an = ExampleAN.new()
       assert_equal("", an.value)
     end
 
--- test/eddy/models/element/id_test.rb
+++ test/eddy/models/element/id_test.rb
@@ -3,11 +3,17 @@ require "test_helper"
 module ModelsTest
 
   # Example class for testing inheritence of `Eddy::Element::ID`
-  class ExampleIdElement < Eddy::Element::ID
+  class ExampleID < Eddy::Element::ID
     def initialize(val: nil, req: nil)
-      @id = @name = @description = "Example"
-      super(min: 2, max: 2, req: req, val: val)
+      @id = @name = @description = @ref = "Example"
+      super(
+        min: 2,
+        max: 2,
+        req: req,
+        val: val,
+      )
     end
+
     def code_list()
       return ["00", "01", "02"]
     end
@@ -16,38 +22,38 @@ module ModelsTest
   class IdTest < Minitest::Test
 
     def test_init
-      id = ExampleIdElement.new()
+      id = ExampleID.new()
       assert_equal("ID", id.type)
     end
 
     def test_setter
-      id = ExampleIdElement.new()
+      id = ExampleID.new()
       id.value = "01"
       assert_equal("01", id.value)
     end
 
     def test_setter_validation
-      id = ExampleIdElement.new()
+      id = ExampleID.new()
       assert_raises(Eddy::Errors::ElementValidationError) { id.value = "ZZ" }
     end
 
     def test_getter_when_mandatory
-      id = ExampleIdElement.new(req: "M", val: "02")
+      id = ExampleID.new(req: "M", val: "02")
       assert_equal("02", id.value)
     end
 
     def test_getter_when_mandatory_and_nil
-      id = ExampleIdElement.new(req: "M")
+      id = ExampleID.new(req: "M")
       assert_raises(Eddy::Errors::ElementNilValueError) { id.value() }
     end
 
     def test_getter_when_optional
-      id = ExampleIdElement.new(req: "O", val: "02")
+      id = ExampleID.new(req: "O", val: "02")
       assert_equal("02", id.value())
     end
 
     def test_getter_when_optional_and_nil
-      id = ExampleIdElement.new(req: "O")
+      id = ExampleID.new(req: "O")
       assert_equal("", id.value())
     end
 
