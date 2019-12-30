3b7f50dd16ded0cde05d33a1dda82fd7318901c3
Clay Dunston <claydunston@gmail.com>
2019-12-30 11:22:07 -0600

    Update build test

--- test/eddy/build/transacton_set_builder_test.rb
+++ test/eddy/build/transacton_set_builder_test.rb
@@ -39,8 +39,22 @@ module BuildTest
     end
 
     def test_transaction_set_builder
+      skip("WIP")
       have = Eddy::Build::TransactionSetBuilder.from_summary(@summary).ginny_class.render()
       assert_equal(@ts855, have)
+    end
+
+    def test_segment_accessor
+      want = <<~RB.strip
+        # (see Eddy::Segments::CTT)
+        # @yieldparam [Eddy::Segments::CTT] ctt
+        # @return [Eddy::Segments::CTT]
+        def CTT()
+          yield(@ctt) if block_given?
+          return @ctt
+        end
+      RB
+      assert_equal(want, Eddy::Build::TransactionSetBuilder.segment_accessor("ctt"))
     end
 
   end
