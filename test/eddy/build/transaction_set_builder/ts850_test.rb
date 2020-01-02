21c96ef2bbde64c984b580c5d9800c3f0ad85243
Clay Dunston <claydunston@gmail.com>
2020-01-02 12:46:12 -0600

    Update fixture for `TS850Test` to use actual file in lib for desired result.

--- test/eddy/build/transaction_set_builder/ts850_test.rb
+++ test/eddy/build/transaction_set_builder/ts850_test.rb
@@ -6,11 +6,10 @@ module BuildTest
 
       def setup
         @summary = Eddy::Schema::TransactionSetSummary.from_file(file_fixture("schema/850.edi.yml"))
-        @ts850 = File.read(file_fixture("ruby/850.rb")).strip
+        @ts850 = File.read(File.join(Eddy::Util.root_dir, "lib/definitions/transaction_sets/manual/850/850.rb")).strip
       end
 
       def test_transaction_set_builder
-        # skip("WIP")
         have = Eddy::Build::TransactionSetBuilder.from_summary(@summary).ginny_class.render()
         assert_equal(@ts850, have)
       end
