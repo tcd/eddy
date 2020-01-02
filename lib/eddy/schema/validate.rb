2426a2fc2d76ba19c051ce220174e91dda1f3bff
Clay Dunston <claydunston@gmail.com>
2020-01-02 15:54:59 -0600

    Add json validation for LoopSummary data

--- lib/eddy/schema/validate.rb
+++ lib/eddy/schema/validate.rb
@@ -15,6 +15,17 @@ module Eddy
       schemer.valid?(data)
     end
 
+    # Check a Loop definition file against Eddy's JSON schema.
+    #
+    # @param path [String] Path to the file.
+    # @return [Boolean]
+    def self.valid_loop_data?(path)
+      schema = Pathname.new(File.join(Eddy::Util.data_dir, "schema", "loop_summary.json"))
+      schemer = JSONSchemer.schema(schema, ref_resolver: "net/http")
+      data = Eddy::Util.read_json_or_yaml(path, symbolize: false)
+      schemer.valid?(data)
+    end
+
     # Check a Transaction Set definition file against Eddy's JSON schema.
     #
     # @param path [String] Path to the file.
