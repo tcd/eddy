bed31481bb3cda3b81aa58a7a32331ff47ad8e23
Clay Dunston <claydunston@gmail.com>
2020-01-04 12:24:56 -0600

    [WIP] CLI tests

--- test/eddy/cli_test.rb
+++ test/eddy/cli_test.rb
@@ -1,4 +1,22 @@
 require "test_helper"
 
 class CLITest < Minitest::Test
+
+  def test_version
+    skip("Figure out how to test cli output.")
+    out, err = capture_io do
+      Eddy::CLI.start(["--version"])
+    end
+    assert_equal("", err)
+    assert_equal("Eddy version #{Eddy::VERSION}", out)
+  end
+
+  def test_segment
+    skip()
+  end
+
+  def test_set
+    skip()
+  end
+
 end
