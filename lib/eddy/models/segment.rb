8a09e11b86bf21b494846355d3c5bef525207896
Clay Dunston <claydunston@gmail.com>
2020-01-08 12:36:41 -0600

    Allow segment skipping

--- lib/eddy/models/segment.rb
+++ lib/eddy/models/segment.rb
@@ -13,6 +13,8 @@ module Eddy
       attr_accessor :elements
       # @return [Eddy::Data::Store] Data passed down from a Transaction Set.
       attr_accessor :store
+      # @return [Boolean] (false) If true, `#render` will return `nil`.
+      attr_accessor :skip
 
       # All of a Segment's elements need to be declared in its constructor.
       #
@@ -20,14 +22,16 @@ module Eddy
       # @param elements [Eddy::Models::Element::Base]
       # @return [void]
       def initialize(store, *elements)
+        self.skip = false
         self.store = store
         elements.flatten!
         self.elements = elements || []
       end
 
       # @param separator [String] (self.store.element_separator) String used to delimit elements within a segment.
-      # @return [String]
+      # @return [String,nil]
       def render(separator = self.store.element_separator)
+        return nil if self.skip
         segment = [self.id, self.elements.map(&:value)].join(separator)
         return Eddy::Util.trim_delims_from_segment(segment, separator: separator)
       end
---
+++ test/eddy/write/skip_test.rb
@@ -0,0 +1,60 @@
+require "test_helper"
+
+module WriteTest
+  class SkipTest < Minitest::Test
+
+    def setup
+      @epoch = Time.at(0).utc()
+      Eddy.clear_data()
+    end
+
+    def test_skipping_a_segment
+      want = <<~EDI.gsub(/\n/, "")
+        ST*850*0001~
+        BEG*00*DS*00000007397108**19700101~
+        N1*ST*Sweeney Todd~
+        N3*2705 Fleet St~
+        N4*Birmingham*AL*35226*US~
+        PO1*1*1*EA*59.95**UP*860001662184*VN*860001662184~
+        CTT*1~
+        SE*9*0001
+      EDI
+      store = Eddy::Data::Store.new(time: @epoch)
+      ts = Eddy::TransactionSets::TS850::TS.new(store)
+      ts.BEG do |beg|
+        beg.TransactionSetPurposeCode = "00"
+        beg.BEG02 = "DS"
+        beg.BEG03 = "00000007397108"
+        beg.BEG05 = @epoch
+      end
+      ts.TD5.skip = true
+      ts.L_N1 do |rep|
+        # N1
+        rep.N1.N101 = "ST"
+        rep.N1.Name = "Sweeney Todd"
+        # N3
+        rep.N3.AddressInformation1 = "2705 Fleet St"
+        # N4
+        rep.N4.CityName = "Birmingham"
+        rep.N4.StateOrProvinceCode = "AL"
+        rep.N4.PostalCode = "35226"
+        rep.N4.CountryCode = "US"
+      end
+      ts.L_PO1 do |rep|
+        # CBD Topical Cream 400mg THC Free
+        rep.PO1.AssignedIdentification = "1"
+        rep.PO1.QuantityOrdered = 1
+        rep.PO1.UnitOrBasisForMeasurementCode = "EA"
+        rep.PO1.UnitPrice = 59.95
+        rep.PO1.ProductServiceIdQualifier1 = "UP"
+        rep.PO1.ProductServiceId1 = "860001662184"
+        rep.PO1.ProductServiceIdQualifier2 = "VN"
+        rep.PO1.ProductServiceId2 = "860001662184"
+      end
+      ts.CTT.NumberOfLineItems = 1
+      result = ts.render()
+      assert_equal(want, result)
+    end
+
+  end
+end
