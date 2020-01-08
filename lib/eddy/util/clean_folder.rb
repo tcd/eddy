4b913b1f20dd07b6a46ef51f36332f9427209e5f
Clay Dunston <claydunston@gmail.com>
2020-01-08 10:51:49 -0600

    Write tests for edi data utility functions

--- lib/eddy/util/clean_folder.rb
+++ lib/eddy/util/clean_folder.rb
@@ -2,10 +2,13 @@ module Eddy
   module Util
     # Delete all files from a folder; returns number of deleted files.
     #
+    # Fails if the folder contains any symlinks.
+    #
     # @param path [String] Path to the folder.
     # @return [Integer]
     def self.clean_folder(path)
       dir = File.expand_path(path)
+      return 0 unless Dir.exist?(dir)
       children = Dir.entries(dir).select { |f| File.file?(File.join(dir, f)) }
       deleted = File.unlink(*children)
       return deleted
--- test/eddy/util/edi_data_test.rb
+++ test/eddy/util/edi_data_test.rb
@@ -1,22 +1,26 @@
+# @!domain Minitest::Expectations
 require "test_helper"
 
 module UtilTest
   class EDIDataTest < Minitest::Test
 
     def test_raw_element_data
-      # data = Eddy::Util.raw_element_data()
-      skip()
+      data = Eddy::Util.raw_element_data()
+      assert_kind_of(Array, data)
+      refute_equal(0, data.length)
     end
 
+    # 1 - 1688, or I01-I64
     def test_element_ids
-      # 1688, or I01-I64
-      # data = Eddy::Util.element_ids()
-      skip()
+      data = Eddy::Util.element_ids()
+      assert_kind_of(Hash, data)
+      refute_equal(0, data.length)
     end
 
     def test_segment_ids
-      # data = Eddy::Util.segment_ids()
-      skip()
+      data = Eddy::Util.segment_ids()
+      assert_kind_of(Hash, data)
+      refute_equal(0, data.length)
     end
 
     def test_element_name_by_id
@@ -25,7 +29,8 @@ module UtilTest
     end
 
     def test_element_name_by_invalid_id
-      skip()
+      err = assert_raises(Eddy::Errors::Error) { Eddy::Util.element_name_by_id("0") }
+      assert_equal("No element found with id 0", err.message)
     end
 
     def test_segment_name_by_id
@@ -34,27 +39,32 @@ module UtilTest
     end
 
     def test_segment_name_by_invalid_id
-      skip()
+      err = assert_raises(Eddy::Errors::Error) { Eddy::Util.segment_name_by_id("0") }
+      assert_equal("No segment found with id 0", err.message)
     end
 
     def test_list_element_classes
-      # data = Eddy::Util.list_element_classes()
-      skip()
+      data = Eddy::Util.list_element_classes()
+      assert_kind_of(Array, data)
+      refute_equal(0, data.length)
     end
 
     def test_list_segment_classes
-      # data = Eddy::Util.list_segment_classes()
-      skip()
+      data = Eddy::Util.list_segment_classes()
+      assert_kind_of(Array, data)
+      refute_equal(0, data.length)
     end
 
     def test_list_segment_definitions
-      # data = Eddy::Util.list_segment_definitions()
-      skip()
+      data = Eddy::Util.list_segment_definitions()
+      assert_kind_of(Array, data)
+      refute_equal(0, data.length)
     end
 
     def test_list_built_elements
-      # data = Eddy::Util.list_built_elements()
-      skip()
+      Eddy::Build.clean_folders()
+      data = Eddy::Util.list_built_elements()
+      assert_equal(0, data.length)
     end
 
   end
