982f9898fb0a31a63b384c31250184b16446a9f9
Clay Dunston <claydunston@gmail.com>
2020-01-17 10:36:18 -0600

    Add notes on composite elements

--- lib/eddy/models/element/composite.rb
+++ lib/eddy/models/element/composite.rb
@@ -2,6 +2,8 @@ module Eddy
   module Models
     module Element
       # - [References to Component Data Elements - X12 RFI](http://www.x12.org/rfis/References%20to%20Component%20Data%20Elements.pdf)
+      # - [What’s the Repetition Separator Character in 5010 X12?](http://www.rdpcrystal.com/whats-the-repetition-separator-character-in-5010-x12/)
+      # - [Delimiters in data fields (RFI 1815)](http://rfi.x12.org/Request/Details/1815?stateViewModel=WPC.RFI.Models.ViewModels.RequestViewModel)
       class Composite; end
     end
   end
